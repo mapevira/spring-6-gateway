ac4923fd32bdd1895a6e52f70eacb95803b1c490
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-31 19:23:34 +0200

    342. Gateway Resource Server Configuration

---
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - raulp
+ * @version 31/08/2024 - 19:10
+ * @since jdk 1.21
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+}
